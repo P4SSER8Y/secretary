acb08cea1cd46962b667ef89855693a855443756
P4SSER8Y <P4SSER8Y@outlook.com>
2023-12-30 00:25:34 +0800

    NEW: support persitent record some data via sled & bincode

---
+++ src/database.rs
@@ -0,0 +1,39 @@
+use anyhow::{Ok, Result};
+use once_cell::sync::Lazy;
+use serde::{de::DeserializeOwned, Serialize};
+
+fn get_db() -> sled::Db {
+    static INSTANCE: Lazy<sled::Db> = Lazy::new(|| sled::open("memory").unwrap());
+    return INSTANCE.clone();
+}
+
+pub struct Db {
+    db: sled::Db,
+}
+
+impl Db {
+    pub fn new() -> Db {
+        Db { db: get_db() }
+    }
+
+    pub fn get<T: DeserializeOwned>(&self, key: &str) -> Result<Option<T>> {
+        let result = self.db.get(key)?;
+        if let Some(raw) = result {
+            let result: T = bincode::deserialize::<T>(&raw)?;
+            return Ok(Some(result));
+        } else {
+            return Ok(None);
+        }
+    }
+
+    pub fn set<T: Serialize>(&self, key: &str, value: &T) -> Result<()> {
+        let value = bincode::serialize(value)?;
+        let _ = self.db.insert(key, value)?;
+        return Ok(());
+    }
+
+    pub fn flush(&self) -> Result<()> {
+        let _ = self.db.flush()?;
+        return Ok(());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-#[macro_use] extern crate rocket;
+use chrono::Local;
+use rocket::{info, error};
 
+#[macro_use]
+extern crate rocket;
+
+mod database;
 mod kindle;
 mod qweather;
 
 #[launch]
 fn rocket() -> _ {
     let wtf = rocket::build();
-    
+
+    let db = database::Db::new();
+    if let Ok(launch) = db.get::<String>("launch") {
+        info!("last launch at {:?}", launch);
+    } else {
+        info!("never launched before");
+    }
+    if let Err(_) = db.set("launch", &Local::now().to_rfc3339()) {
+        error!("last launch not found");
+    }
+    let _ = db.flush();
+
     let wtf = qweather::build(wtf);
     let wtf = kindle::build("/kindle", wtf);
     wtf
